d9c261966527d6bcbf7d1b465566baf271d3fb74
Justin Yun <justinyun@google.com>
2019-11-15 16:08:00 +0900

    v7101o: Inherit base system_ext.mk files for aosp targets
    Make aosp targets inherit handheld_system_ext.mk and
    telephony_system_ext.mk files.
    Devices that have /system_ext or /system/system_ext must inherit any
    of *_system_ext.mk files to install mandatory packages for system_ext
    partition.
    
    Bug: 144542478
    Test: Build aosp targets

--- aosp_v7101o.mk
+++ aosp_v7101o.mk
@@ -9,6 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
 # Inherit from v7101o device
 $(call inherit-product, device/teracube/v7101o/device.mk)
 

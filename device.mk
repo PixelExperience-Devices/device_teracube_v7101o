4220766afd7839e9c8c11fb4399fb9c36c1105b3
maxwen <max.weninger@gmail.com>
2020-08-17 03:20:10 +1000

    v7101o: overlay-aosp: Enable config_haveHigherAspectRatioScreen

--- device.mk
+++ device.mk
@@ -145,7 +145,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp
 
 # Permissions
 PRODUCT_COPY_FILES += \

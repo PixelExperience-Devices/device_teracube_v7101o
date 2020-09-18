08dcad57cbc808b46dad86517bd3e9aac8a665c0
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    v7101o: Build libhidltransport/libhwbinder
    * Needed for old blobs

--- device.mk
+++ device.mk
@@ -40,6 +40,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhwbinder
+
 # ImsInit hack
 PRODUCT_PACKAGES += \
     ImsInit

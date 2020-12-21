3543046166556aa159b2ca4a8fa55c270b85f3c7
TheMalachite <eliasgheeraert@gmail.com>
2020-12-21 15:08:03 +0100

    Revert "v7101o: Build AOSP FM app"
    This reverts commit 051d4de7fe86da461c33515820bb421872ceba8c.
    lbfmjni can't be builded since it's a mediatek phone

--- device.mk
+++ device.mk
@@ -42,11 +42,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.controls.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.controls.xml
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \

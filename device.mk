a6ac6847b34641f5079996c5063c53b22c643b3c
TheMalachite <eliasgheeraert@gmail.com>
2021-04-08 23:55:06 +0200

    v7101o: Import public.libraries.txt from stock
    - Taken from Teracube A10_BETA-1

--- device.mk
+++ device.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \

5869e9e5a7334e974c613735280cda46d0d83b72
TheMalachite <eliasgheeraert@gmail.com>
2021-04-11 19:01:01 +0200

    v7101o: releasetools: Be more clear about dtbo flashing

--- releasetools.py
+++ releasetools.py
@@ -16,26 +16,22 @@
 
 import common
 
-
 def FullOTA_InstallEnd(info):
-    OTA_InstallEnd(info, False)
-
+  OTA_InstallEnd(info, False)
 
 def IncrementalOTA_InstallEnd(info):
-    OTA_InstallEnd(info, True)
-
+  OTA_InstallEnd(info, True)
 
 def AddImage(info, basename, dest, incremental):
-    name = basename
-    if incremental:
-        input_zip = info.source_zip
-    else:
-        input_zip = info.input_zip
-    data = input_zip.read("IMAGES/" + basename)
-    common.ZipWriteStr(info.output_zip, name, data)
-    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
-
+  name = basename
+  if incremental:
+    input_zip = info.source_zip
+  else:
+    input_zip = info.input_zip
+  data = input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info, incremental):
-    AddImage(info, "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo", incremental)
+  info.script.Print("Flashing dtbo image...")
+  AddImage(info, "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo", incremental)

012d97c35391ef26f9e1058040338363906d64af
TheMalachite <eliasgheeraert@gmail.com>
2021-04-11 19:26:50 +0200

    Revert "v7101o: Stop forcing AOSP recovery"
    This reverts commit 085c72a94f2a36075b0f7fcad2ecd0e8f1705e59.
    Can't manage to decrypt data with TrustKernel TEE

--- aosp_v7101o.mk
+++ aosp_v7101o.mk
@@ -19,6 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/teracube/v7101o/device.mk)
 
 # Inherit some common PixelExperience stuff.
+TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 

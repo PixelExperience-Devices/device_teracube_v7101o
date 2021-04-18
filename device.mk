fa9d3f293cd8368db9865208916336e1eea04019
TheMalachite <eliasgheeraert@gmail.com>
2021-04-19 01:28:44 +0200

    v7101o: Switch back to prebuilt IMS stack

--- device.mk
+++ device.mk
@@ -40,6 +40,19 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
 # ImsInit hack
 PRODUCT_PACKAGES += \
     ImsInit

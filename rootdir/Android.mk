241d722e26cb6227d49d12bb0a46f05473f4e04e
TheMalachite <eliasgheeraert@gmail.com>
2021-04-28 12:00:43 +0200

    v7101o: rootdir: Cleanup ueventd.mt6771.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -109,6 +109,6 @@ LOCAL_MODULE       := ueventd.mt6771.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.mt6771.rc
+LOCAL_SRC_FILES    := etc/ueventd.mt6771.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)

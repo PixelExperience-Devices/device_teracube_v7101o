ac88072c9115aa235cad41f2e82c108b9babb93a
TheMalachite <eliasgheeraert@gmail.com>
2021-04-28 03:08:21 +0200

    v7101o: Update DRM Widevine HAL from Note_11P

--- device.mk
+++ device.mk
@@ -68,9 +68,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Gatekeeper
 PRODUCT_PACKAGES += \

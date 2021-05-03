cf90998e3db348b30f38c4c864a68444f54fc4db
TheMalachite <eliasgheeraert@gmail.com>
2021-05-04 01:24:51 +0200

    v7101o: libshims: Switch to blueprint

--- libshims/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE      := libshim_vtservice
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES   := vtservice/libshim_vtservice.cpp
-include $(BUILD_SHARED_LIBRARY)
